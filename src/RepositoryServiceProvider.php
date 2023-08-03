96ede071acab48186bb897b069a4d31129038f50
HANKSIE <32055793+HANKSIE@users.noreply.github.com>
2023-08-04 06:03:14 +0800

    fix(service provider) remove doesnt exist class binding.

--- src/RepositoryServiceProvider.php
+++ src/RepositoryServiceProvider.php
@@ -1,13 +1,12 @@
 <?php
 namespace Hanksie\Repository;
 
-use Hanksie\Repository\Helpers\Pipeline as PipelineHelper;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->instance(PipelineHelper::class, PipelineHelper::class);
+        //
     }
 }
